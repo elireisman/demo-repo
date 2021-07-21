1ccd258d1f4aa2e30ee43fbe2dace6ad2a2df3fd
Eli Reisman <elireisman@github.com>
2021-07-21 08:38:21 -0700

    add business plan

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 	"github.com/ethereum/go-ethereum/core"
 )
 
-// utilize vulnerable import
+// Business Plan:
+// 1. blockchain
+// 2. ???
+// 3. profit!
 func main() {
+	// TODO: step 2
 	bc := new(core.BlockChain)
 	fmt.Printf("%+v\n", bc)
 }
